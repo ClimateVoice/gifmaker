af6e7ca4e74c8c0483901a13f1939de7c4bdfab3
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-02-12 14:47:18 -0500

    Add files via upload

---
+++ create_image.py
@@ -0,0 +1,10 @@
+import Image
+
+background = Image.open("1.png")
+overlay = Image.open("3.jpg")
+
+background = background.convert("RGBA")
+overlay = overlay.convert("RGBA")
+
+new_img = Image.blend(background, overlay, 0.5)
+new_img.save("new.png","PNG")
